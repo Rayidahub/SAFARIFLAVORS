ae38f5ea20af74603a36c97925471c8e70a26ecf
Raymond Gaius Yidawi <rayidatech@gmail.com>
2023-10-24 20:24:32 +0100

    Little changes made on app.py👍

--- Backend/app.py
+++ Backend/app.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager, login_user, login_required, logout_user, c
 from models.schema import Region, SubRegion, Country, Recipe
 from models.user import User
 
-app = Flask(__name)
+app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Change this to a secure secret key
 
 # MongoDB configuration
