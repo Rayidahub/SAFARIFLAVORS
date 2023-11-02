4cb74d243581829545aff2668243da50964a54af
Clive Sasaka <clivesasaka@gmail.com>
2023-11-02 19:09:47 +0300

    Draft site

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -24,3 +24,30 @@ document.querySelectorAll('.country-item').forEach(country => {
        // Change images according to the selected country
    });
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+    
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+var foodImages = document.getElementsByClassName("food-images")[0].getElementsByTagName("img");
+for (var i = 0; i < foodImages.length; i++) {
+    foodImages[i].onclick = function() {
+        modal.style.display = "block";
+        document.getElementById("food-name").innerHTML = this.alt;
+        document.getElementById("food-description").innerHTML = "Description for " + this.alt;
+    }
+}
