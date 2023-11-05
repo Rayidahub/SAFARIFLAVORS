2decc02f499bb44c9cd49f9ac1476d53bba66658
humphreymbugua <humphreeydiaz@gmail.com>
2023-11-05 18:47:38 +0300

    Added the end block.

--- SafariFlavors/test_app.py
+++ SafariFlavors/test_app.py
@@ -4,5 +4,9 @@ from app import app
 class TestApp(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
-        self.app.testing = True
+self.app.testing = True
+
+
+if __name__ == '__main__':
+    unittest.main()
 
