ccd314d04d3d16e53e63d813efcf2b1e7eb2f985
Clive Sasaka <clivesasaka@gmail.com>
2023-11-08 19:56:49 +0300

    Spoonacular API

--- SafariFlavors/app.py
+++ SafariFlavors/app.py
@@ -8,6 +8,8 @@ from models.schema import Region, SubRegion, Country, Recipe
 from models.user import User
 from models.model import Food
 import os
+import requests
+from urllib.parse import unquote
 
 
 app = Flask(__name__)
@@ -15,6 +17,9 @@ app.config['MONGODB_SETTINGS'] = {
     'db': 'recipe_db',
     'host': 'mongodb://localhost:27017/recipe_db'   
 }
+
+# Set up the configuration for your API key from https://spoonacular.com/food-api
+API_KEY="2180a4bc65994688bdfedeb311c65ac8"
 
 # Initialize the MongoEngine
 db = MongoEngine(app)
@@ -61,11 +66,70 @@ def allowed_file(filename):
 def load_user(user_id):
     return User.objects(pk=user_id).first()
 
-@app.route("/")
+@app.route('/home', methods=['GET'])
 def home():
-     return render_template('home.html')
+    # Render the main page with empty recipe list and search query
+    return render_template('home.html', recipes=[], search_query='')
 
-@app.route('/')
+def search_recipes(query):
+    url = f'https://api.spoonacular.com/recipes/complexSearch'
+    params = {
+        'apiKey': API_KEY,
+        'query': query,
+        'number': 10,
+        'instructionsRequired': True,
+        'addRecipeInformation': True,
+        'fillIngredients': True,
+    }
+
+    # Send a GET request to the Spoonacular API with the query parameters
+    response = requests.get(url, params=params)
+    # If the API call is successful
+    if response.status_code == 200:
+        # Parse the API response as JSON data
+        data = response.json()
+        # Return the list of recipe results
+        return data['results']
+    # If the API call is not successful
+    return []
+
+@app.route('/', methods=['GET', 'POST'])
+def display_recipes():
+    if request.method == 'POST':
+        # If a form is submitted
+        query = request.form.get('search_query', '')
+        # Perform a search for recipes with the given query
+        recipes = search_recipes(query)
+        # Render the main page with the search results and the search query
+        return render_template('home.html', recipes=recipes, search_query=query)
+    
+    # If it's a GET request or no form submitted
+    search_query = request.args.get('search_query', '')
+    decoded_search_query = unquote(search_query)
+    # Perform a search for recipes with the decoded search query
+    recipes = search_recipes(decoded_search_query)
+    # Render the main page
+    return render_template('home.html', recipes=recipes, search_query=decoded_search_query)
+
+# Route to view a specific recipe with a given recipe ID
+@app.route('/recipe/<int:recipe_id>')
+def view_recipe(recipe_id):
+    # Get the search query from the URL query parameters
+    search_query = request.args.get('search_query', '')
+    # Build the URL to get information about the specific recipe ID from Spoonacular API
+    url = f'https://api.spoonacular.com/recipes/{recipe_id}/information'
+    params = {
+        'apiKey': API_KEY,
+    }
+
+    # Send a GET request to the Spoonacular API to get the recipe information
+    response = requests.get(url, params=params)
+    # If the API call is successful
+    if response.status_code == 200:
+        recipe = response.json()
+        return render_template('recipe.html', recipe=recipe, search_query=search_query)
+    return "Recipe not found!", 404
+
 @app.route('/about/')
 def index():
     # Fetch regions, subregions, or any other data you want to display
@@ -82,6 +146,7 @@ def country_recipe(country_name):
     else:
         # Handle the case where the country is not found
         return render_template('recipe.html', country_name=country_name), 404
+
 @app.route('/recipe/submit-food', methods=['POST'])
 def submit_food():
     # Get data from the form
